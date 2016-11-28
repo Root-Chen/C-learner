70a98485102fa007253dd8cbd8f812393c700263
Root-Chen <root_chen781159385@163.com>
2016-11-28 16:22:40 +0800

    生成一个second.cpp
    生成一个second.cpp

---
+++ second.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "这是新创建的CPP文件" << endl;
+    cout << "第一次向github提交项目，记得要把分支合并到master上去，项目才生效." << endl;
+    cout << "hello world!" << endl;
+    return 0;
+}
